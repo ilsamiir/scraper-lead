e66ba56d9e5418706751a05b20a4015a3eb10c54
ilsamiir <angelogiaccheti@gmail.com>
2026-03-27 18:53:15 +0100

    WIP

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "../styles/btn-brand-orange.css";
 
 const inter = Inter({
   variable: "--font-inter",
